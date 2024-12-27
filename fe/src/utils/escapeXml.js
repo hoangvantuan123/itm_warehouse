1d6290bc45dcd1eb2cacfafab7935742b8ddc34d
hoangvantuan123 <rammatuan@gmail.com>
2024-12-27 08:42:36 +0700

    update

--- fe/src/utils/escapeXml.js
+++ fe/src/utils/escapeXml.js
@@ -1,20 +1,22 @@
 export const escapeXml = (str) => {
-    return str.replace(/[&<>"']/g, (char) => {
+  if (typeof str !== 'string') {
+      console.warn('escapeXml received a non-string value:', str);
+      str = str == null ? '' : String(str); 
+  }
+  return str.replace(/[&<>"']/g, (char) => {
       switch (char) {
-        case '&':
-          return '&amp;';
-        case '<':
-          return '&lt;';
-        case '>':
-          return '&gt;';
-        case '"':
-          return '&quot;';
-        case "'":
-          return '&apos;';
-        default:
-          return char;
+          case '&':
+              return '&amp;';
+          case '<':
+              return '&lt;';
+          case '>':
+              return '&gt;';
+          case '"':
+              return '&quot;';
+          case "'":
+              return '&apos;';
+          default:
+              return char;
       }
-    });
-  };
-  
-  
+  });
+};
